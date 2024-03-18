026d15427225d11fa24d4d080538a88041b98cdd
BattlePancake <vladsivograkovwork@gmail.com>
2024-03-18 12:35:55 +0300

    'Homework' program
    New program 'Welcome questionary' added

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter your name");     
+string name = Console.ReadLine();
+Console.WriteLine("Enter your surname");
+string surname = Console.ReadLine();  
+Console.WriteLine("Enter your age");
+int age = int.Parse(Console.ReadLine());                                 
+Console.WriteLine($"Hello, your last name is {surname} and first name is {name} and you are {age} years old"); 
